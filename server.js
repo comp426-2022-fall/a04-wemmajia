13e6c148cfc05f7d2e890eca122ea121eb3dd52d
Emma Jia <emmajia@unc.edu>
2022-11-29 12:54:44 -0500

    dice

--- server.js
+++ server.js
@@ -19,3 +19,11 @@ app.get('/app/roll/', (req, res) => {
 app.get('/app/roll/', (req, res) => {
     res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)))
 })
+
+app.get('/app/roll/:sides/', (req, res) => {
+    res.send(roll(parseInt(req.params.sides), 2, 1))
+})
+
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1))
+})
