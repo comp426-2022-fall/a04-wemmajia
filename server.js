06fd45cc7401b786fb7d0ae5b56fc3147dab33fc
Emma Jia <emmajia@unc.edu>
2022-11-29 12:45:06 -0500

    express

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ import express from "express";
 import { roll } from "./lib/roll.js";
 
 const args = minimist(process.argv.slice(2));
-
 const port = args.port || 5000;
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+
