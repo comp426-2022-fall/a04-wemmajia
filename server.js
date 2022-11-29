7c405dff62b0d144144dec6e351a8b8b3f48c078
Emma Jia <emmajia@unc.edu>
2022-11-29 12:50:37 -0500

    app, app roll

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ const port = args.port || 5000;
 const app = express();
 app.use(express.urlencoded({extended: true}));
 
+app.get('/app/', (req, res) => {
+    res.send('200 OK')
+})
 
+app.get('/app/roll/', (req, res) => {
+    res.send(roll(6, 2, 1))
+})
+
+app.get('/app/roll/', (req, res) => {
+    res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)))
+})
