c791765a80c057390cee3b630561871e1340f0f9
Emma Jia <emmajia@unc.edu>
2022-11-29 12:29:34 -0500

    args

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ import minimist from "minimist";
 import express from "express";
 import { roll } from "./lib/roll.js";
 
+const args = minimist(process.argv.slice(2));
+
+const port = args.port || 5000;
